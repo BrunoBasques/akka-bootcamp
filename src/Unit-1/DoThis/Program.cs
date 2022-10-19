65f06a02736bad48bcab902567d07f712c2a41a8
Bruno Augusto Basques Ramos Camargo Rodrigues <brunocr@ciandt.com>
2022-10-19 14:58:14 -0300

    Unit 1 / Lesson 1

--- src/Unit-1/DoThis/Program.cs
+++ src/Unit-1/DoThis/Program.cs
@@ -11,18 +11,19 @@ namespace WinTail
         static void Main(string[] args)
         {
             // initialize MyActorSystem
-            // YOU NEED TO FILL IN HERE
+            MyActorSystem = ActorSystem.Create("MyActorSystem");
 
             PrintInstructions();
 
             // time to make your first actors!
-            //YOU NEED TO FILL IN HERE
-            // make consoleWriterActor using these props: Props.Create(() => new ConsoleWriterActor())
-            // make consoleReaderActor using these props: Props.Create(() => new ConsoleReaderActor(consoleWriterActor))
+            IActorRef consoleWriterActor = MyActorSystem.ActorOf(
+                Props.Create(() => new ConsoleWriterActor()), "consoleWriterActor");
+            IActorRef ConsoleReaderActor = MyActorSystem.ActorOf(
+                Props.Create(() => new ConsoleReaderActor(consoleWriterActor)), "consoleReaderActor");
 
 
             // tell console reader to begin
-            //YOU NEED TO FILL IN HERE
+            ConsoleReaderActor.Tell("start");
 
             // blocks the main thread from exiting until the actor system is shut down
             MyActorSystem.WhenTerminated.Wait();
@@ -32,11 +33,11 @@ namespace WinTail
         {
             Console.WriteLine("Write whatever you want into the console!");
             Console.Write("Some lines will appear as");
-            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.ForegroundColor = ConsoleColor.Magenta;
             Console.Write(" red ");
             Console.ResetColor();
             Console.Write(" and others will appear as");
-            Console.ForegroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.White;
             Console.Write(" green! ");
             Console.ResetColor();
             Console.WriteLine();
